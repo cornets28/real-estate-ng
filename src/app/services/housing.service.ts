426680c5a4cc3a212103cad9f481cece268ba471
Samuel Cornet <corsam28@gmail.com>
2023-09-21 12:00:20 -0500

    Modify HTTP Data using Pipe

--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { IProperty } from '../property/IProperty.interface';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +11,20 @@ export class HousingService {
 
   constructor(private http: HttpClient) { }
 
-  getAllProperties() {
+  getAllProperties(): Observable<IProperty[]> {
     return this.http.get('data/properties.json')
+    // convert properties into array   
+    .pipe(
+      map(data => {
+        const propertiesArray: Array<IProperty> = [];
+        for (const id in data) {
+          if (data.hasOwnProperty(id)) {
+            // @ts-ignore
+            propertiesArray.push(data[id]);
+          }
+        }
+        return propertiesArray;
+      })
+    )
   }
 }
