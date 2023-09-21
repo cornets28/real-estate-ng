609026c8cc8455065cbb2f58e9b8900444294c87
Samuel Cornet <corsam28@gmail.com>
2023-09-21 00:50:15 -0500

    Create the property list and add CSS animation to card

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./property-card.component.css']
 })
 export class PropertyCardComponent {
-  Property: any = {
-    "Id": 1,
-    "Name": "Home by the Library",
-    "Type": "House",
-    "Price": 1200,
-  }
+ @Input() property: any
 }
