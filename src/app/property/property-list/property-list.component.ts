0e766542a60b7f43ca30f7377a02e8ee4a69a36a
Samuel Cornet <corsam28@gmail.com>
2023-09-29 10:17:23 -0500

    Add filter and sort

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HousingService } from 'src/app/services/housing.service';
 import { ActivatedRoute } from '@angular/router';
+import { IPropertyBase } from 'src/app/model/ipropertybase';
 
 @Component({
   selector: 'app-property-list',
@@ -8,26 +9,46 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-  SellRent = 1
-  // @ts-expect-error
-  properties: Array<IPropertyBase>;
+  SellRent = 1;
+  properties!: IPropertyBase[];
+  Today = new Date();
+  City = '';
+  SearchCity = '';
+  SortbyParam = '';
+  SortDirection = 'asc';
 
-  constructor(private route: ActivatedRoute, private housingSevice: HousingService) { }
+  constructor(private route: ActivatedRoute, private housingService: HousingService) { }
 
   ngOnInit(): void {
     if (this.route.snapshot.url.toString()) {
-      this.SellRent = 2; // Means we are on rent-property URL else we are on the base URL
+      this.SellRent = 2; // Means we are on rent-property URL else we are on base URL
     }
-    this.housingSevice.getAllProperties(this.SellRent).subscribe(
-      data => {
+    this.housingService.getAllProperties(this.SellRent).subscribe(
+        data => {
         this.properties = data;
         console.log(data);
-        console.log(this.route.snapshot.url.toString())
       }, error => {
-        console.log('http error')
-        console.log(error)
+        console.log('httperror:');
+        console.log(error);
       }
-    )
+    );
+  }
+
+  onCityFilter() {
+    this.SearchCity = this.City;
+  }
+
+  onCityFilterClear() {
+    this.SearchCity = '';
+    this.City = '';
+  }
+
+  onSortDirection() {
+    if (this.SortDirection === 'desc') {
+      this.SortDirection = 'asc';
+    } else {
+      this.SortDirection = 'desc';
+    }
   }
 
 }
