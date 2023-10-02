3a24499dfb106cb9e70615b403466a3fadcf5ead
Samuel Cornet <corsam28@gmail.com>
2023-10-02 16:31:18 -0500

    Use DTO to show the desired data

--- WebAPI/Controllers/CityController.cs
+++ WebAPI/Controllers/CityController.cs
@@ -1,7 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+// using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
-using WebAPI.Models;
+using WebAPI.Dtos;
 using WebAPI.Interfaces;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -19,13 +26,26 @@ namespace WebAPI.Controllers
         public async Task<IActionResult> GetCities()
         {
             var cities = await uow.CityRepository.GetCitiesAsync();
-            return Ok(cities);
+
+            var citiesDto = from c in cities
+                select new CityDto ()
+                {
+                    Id = c.Id,
+                    Name = c.Name
+                };
+            return Ok(citiesDto);
         }
 
         // POST api/city/post --Post the data in JSON Format
         [HttpPost("post")]
-        public async Task<IActionResult> AddCity(City city)
-        {
+        public async Task<IActionResult> AddCity(CityDto cityDto)
+        { 
+            var city = new City {
+                Name = cityDto.Name,
+                LastUpdatedOn = System.DateTime.Now,
+                LastUpdatedBy = 1
+            };
+
             uow.CityRepository.AddCity(city);
             await uow.SaveAsync();
             return StatusCode(201);
