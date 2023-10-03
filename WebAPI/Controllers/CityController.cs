048e8248d3f8d7a37c15621282b98e097f204c7c
Samuel Cornet <corsam28@gmail.com>
2023-10-03 13:53:51 -0500

    Make use of PUT and PATCH methods

--- WebAPI/Controllers/CityController.cs
+++ WebAPI/Controllers/CityController.cs
@@ -34,7 +34,7 @@ namespace WebAPI.Controllers
             return Ok(citiesDto);
         }
 
-        // PUT api/city/updateCitiName/1 
+        // PUT api/city/updateCitiName/2 
         [HttpPut("updateCitiName/{id}")]
         public async Task<IActionResult> UpdateCity(int id, CityDto cityDto) {
             var cityFromDb = await uow.CityRepository.FindCity(id);
