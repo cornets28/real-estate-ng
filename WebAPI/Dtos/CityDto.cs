46853e63abef57303e1437d6dc5f12ad1d904946
Samuel Cornet <corsam28@gmail.com>
2023-10-04 16:07:18 -0500

    Add more features to Custom Middleware

--- WebAPI/Dtos/CityDto.cs
+++ WebAPI/Dtos/CityDto.cs
@@ -1,14 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebAPI.Dtos
 {
     public class CityDto
     {
-         public int Id { get; set; }
-          public string Name { get; set; }
-          public string Country { get; set; }
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "Name is mandatory field")]
+        [StringLength (50, MinimumLength = 2)]
+        [RegularExpression(".*[a-zA-Z]+.*", ErrorMessage = "Only numerics are not allowed")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Country { get; set; }
     }
 }
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -11,11 +11,18 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using WebAPI.Middlewares;
+
+using System.Net;
 using WebAPI.Models;
 using WebAPI.Data;
 using WebAPI.Data.Repo;
 using WebAPI.Interfaces;
 using WebAPI.Helpers;
+using WebAPI.Extensions;
+
 
 namespace WebAPI
 {
@@ -47,18 +54,17 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
-            }
+        
+            app.ConfigureExceptionHandler(env); 
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
 
             app.UseRouting();
 
-            app.UseAuthorization();
+             app.UseCors(m => m.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
-            app.UseCors(m => m.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
