bd86a0f4ccc924d4335790b6d17e2b12581b2e87
Samuel Cornet <corsam28@gmail.com>
2023-10-14 12:50:28 -0500

    Hookup Property Detail view to WebAPI

--- Client/src/app/services/housing.service.ts
+++ Client/src/app/services/housing.service.ts
@@ -24,11 +24,12 @@ export class HousingService {
 
   // Get single property by its id
   getProperty(id: number) {
-    return this.getAllProperties(1).pipe(
-      map(propertyArray => {
-        return propertyArray.find(p => p.id === id);
-      })
-    )
+    return this.http.get<Property[]>(this.baseUrl + '/property/detail/' + id.toString()) ;
+    // return this.getAllProperties(1).pipe(
+    //   map(propertyArray => {
+    //     return propertyArray.find(p => p.id === id);
+    //   })
+    // )
   }
 
   getAllProperties(SellRent?: number): Observable<Property[]> {
@@ -116,5 +117,31 @@ export class HousingService {
   //     return 101;
   //   }
   // }
+
+  getPropertyAge(dateofEstablishment: string): string
+  {
+      const today = new Date();
+      const estDate = new Date(dateofEstablishment);
+      let age = today.getFullYear() - estDate.getFullYear();
+      const m = today.getMonth() - estDate.getMonth();
+
+      // Current month smaller than establishment month or
+      // Same month but current date smaller than establishment date
+      if (m < 0 || (m === 0 && today.getDate() < estDate.getDate())) {
+          age --;
+      }
+
+      // Establshment date is future date
+      if(today < estDate) {
+          return '0';
+      }
+
+      // Age is less than a year
+      if(age === 0) {
+          return 'Less than a year';
+      }
+
+      return age.toString();
+  }
 }
 
